d27be3c699fc408d45476c9970059e75ffbe7862
Vaibhav Krishna U <vaibhavkrishna0607@gmail.com>
2025-02-07 16:23:36 +0530

    V

--- routes.js
+++ routes.js
@@ -1,6 +1,27 @@
+
 const express = require('express');
 const router = express.Router();
-const { createHandler, readHandler, updateHandler, deleteHandler } = require('./handlers');
+
+const createHandler = (req, res) => {
+    
+    res.status(201).send('Resource created successfully');
+};
+
+const readHandler = (req, res) => {
+    
+    res.status(200).send('Resource data');
+};
+
+
+const updateHandler = (req, res) => {
+  
+    res.status(200).send('Resource updated successfully');
+};
+
+const deleteHandler = (req, res) => {
+    
+    res.status(200).send('Resource deleted successfully');
+};
 
 router.post('/create', createHandler);
 router.get('/read', readHandler);
