7c1cf377b2548df0507f8eaa2ae61f2693fcc988
VaibhavKrishna0607 <vaibhavkrishna0607@gmail.com>
2025-03-17 14:37:31 +0530

    connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+
+
+
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error('Database not connected:', err));
 
 app.get('/ping', (req, res) => {
   res.send('Pong!');
